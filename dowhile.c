029d6747dbdeb0051181500e7db802187129cf5c
PriyanshiShripal <priyanshishripal547@gmail.com>
2025-08-26 10:11:36 +0530

    Add files via upload

---
+++ dowhile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main() {
+     int i=1;
+     printf("First 10 natural numbers using do while loop:\n");
+     do{
+            printf("%d\n",i);
+            i++;
+     } while (i<=10);
+     return 0;
+ }
